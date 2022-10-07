a2c2eff2408434db05993ea3259c6408c576ea57
Nuongho1603 <nuongho.160304@gmail.com>
2022-10-07 19:52:05 +0700

    update

--- module_2/module-2/src/ss3/exercise/StringCount.java
+++ module_2/module-2/src/ss3/exercise/StringCount.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 public class StringCount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Moi ban nhap chuoi:");
+        String str = scanner.nextLine();
+        char[] ch = str.toCharArray();
         System.out.print("Moi ban nhap mot ky tu:");
-        char n = scanner.nextLine().charAt(0);
+        char character = scanner.nextLine().charAt(0);
 //        char chs = (char)n;
 //        char[] chars = chs.toCharArray() ;
-        String s1 = "Nong Quoc Trung";
-        char[] ch = s1.toCharArray();
+//        String s1 = "Nong Quoc Trung";
+
         int count = 0;
 //        System.out.println("Char Array length: " + ch.length);
         System.out.println("Char Array elements: ");
         for (int i = 0; i < ch.length; i++) {
 //            System.out.println(ch[i]);
-            if (ch[i] == n) {
+            if (ch[i] == character) {
                 count++;
             }
 
