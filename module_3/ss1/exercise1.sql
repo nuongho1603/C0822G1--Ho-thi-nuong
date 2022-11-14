6e101d484981332e1b601fb2061468d7643ac787
Nuongho1603 <nuongho.160304@gmail.com>
2022-11-14 16:52:20 +0700

    update first day mdl3

--- module_3/ss1/exercise1.sql
+++ module_3/ss1/exercise1.sql
@@ -1,1 +1,29 @@
 create database exercise1;
+use exercise1;
+create table class(
+  id int primary key, 
+  `name` varchar(255)
+);
+insert into class value (3, "Diễm");
+update 
+  class 
+set 
+  name = 'lan' 
+where 
+  id = 1;
+select 
+  * 
+from 
+  Class;
+create table teacher(
+  id int primary key, 
+  `name` varchar(255), 
+  age int, 
+  country varchar(255)
+);
+select 
+  * 
+from 
+  teacher;
+drop 
+  table class;
