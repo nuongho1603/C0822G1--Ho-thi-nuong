cb6eb964d51731922005b026743ded089cd31573
Nuongho1603 <nuongho.160304@gmail.com>
2022-11-17 22:55:47 +0700

    update

--- module_3/furama_resort/furama_query.sql
+++ module_3/furama_resort/furama_query.sql
@@ -40,8 +40,8 @@ order by so_luong_dat_phong;
  hd.ngay_lam_hop_dong,
  hd.ngay_ket_thuc,
  (dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien
- from hop_dong as hd 
- right join khach_hang as kh on hd.ma_khach_hang = kh.ma_khach_hang
+ from khach_hang as kh
+ right join hop_dong as hd  on hd.ma_khach_hang = kh.ma_khach_hang
  inner join loai_khach as lk on lk.ma_loai_khach = kh.ma_loai_khach
  inner join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu 
  inner join hop_dong_chi_tiet as hdct
@@ -49,19 +49,33 @@ order by so_luong_dat_phong;
  group by kh.ma_khach_hang,hd.ma_hop_dong 
  order by kh.ma_khach_hang;
  
-  select kh.ma_khach_hang,
- kh.ho_ten,
- lk.ten_loai_khach,
- hd.ma_hop_dong,
- dv.ten_dich_vu,
- hd.ngay_lam_hop_dong,
- hd.ngay_ket_thuc,
- (dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien
- from hop_dong as hd 
- right join khach_hang as kh on hd.ma_khach_hang = kh.ma_khach_hang
- left join loai_khach as lk on lk.ma_loai_khach = kh.ma_loai_khach
- left join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu 
- left join hop_dong_chi_tiet as hdct
- left join dich_vu_di_kem as dvdk
- group by kh.ma_khach_hang,hd.ma_hop_dong 
- order by kh.ma_khach_hang;
+ -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của 
+ -- tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+ select dv.ma_dich_vu,dv.ten_dich_vu,dv.dien_tich,dv.chi_phi_thue, ldv.ten_loai_dich_vu
+ from dich_vu as dv
+ inner join loai_dich_vu as ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+ inner join hop_dong as hd on hd.ma_dich_vu = dv.ma_dich_vu
+ where month(hd.ngay_lam_hop_dong) not in (1,2,3)
+ group by dv.ten_dich_vu;
+ 
+ -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+ -- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+ select dv.ma_dich_vu, dv.ten_dich_vu,dv.dien_tich, dv.so_nguoi_toi_da,dv.chi_phi_thue,ldv.ten_loai_dich_vu
+ from dich_vu as dv
+ inner join loai_dich_vu as ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+  inner join hop_dong as hd on hd.ma_dich_vu = dv.ma_dich_vu
+  where year(hd.ngay_lam_hop_dong) not in (2021)
+  group by dv.ma_dich_vu;
+ 
+ -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+select kh.ho_ten
+from khach_hang kh
+group by kh.ho_ten
+having count(kh.ho_ten) >1;
+
+-- 9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+select month(hd.ngay_lam_hop_dong) as thang,count(hd.ma_khach_hang) as so_luong_khach_hang
+from hop_dong as hd
+group by hd.ma_khach_hang
+order by thang;
