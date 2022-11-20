4452b432deea490e224926d1177bb29425e34b14
Nuongho1603 <nuongho.160304@gmail.com>
2022-11-21 01:42:09 +0700

    update

--- module_3/furama_resort/furama_query.sql
+++ module_3/furama_resort/furama_query.sql
@@ -110,7 +110,13 @@ where lk.ten_loai_khach = "Diamond" and kh.dia_chi like "%Vinh%" or kh.dia_chi l
 -- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem 
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
 -- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
-
+select hd.ma_hop_dong,nv.ho_ten,kh.ho_ten,kh.so_dien_thoai,dv.ten_dich_vu,sum(hdct.so_luong) as so_luong_dich_vu_di_kem,hd.tien_dat_coc
+from hop_dong as hd
+join nhan_vien as nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+join khach_hang as kh on hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu as dv on hd.ma_dich_vu = dv.ma_dich_vu
+join hop_dong_chi_tiet as hdct on hd.ma_hop_dong =hdct.ma_hop_dong
+where month(hd.ngay_lam_hop_dong) in (10,11,12) and year(hd.ngay_lam_hop_dong) = 2020 and hd.ma_dich_vu not in(select ma_dich_vu from hop_dong where month(ngay_lam_hop_dong)<=6 and year(ngay_lam_hop_dong)=2021);
 
 -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
