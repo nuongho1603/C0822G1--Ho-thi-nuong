bc60951cf42fee3c095fa22d87fee815155a75f4
Nuongho1603 <nuongho.160304@gmail.com>
2023-01-02 19:11:56 +0700

    u

--- module_4/s11_web_service/exercise/src/main/java/com/controller/BlogRestController.java
+++ module_4/s11_web_service/exercise/src/main/java/com/controller/BlogRestController.java
@@ -25,7 +25,6 @@ public class BlogRestController {
     @GetMapping("")
     public ResponseEntity<List<Blog>> getListBlog() {
         List<Blog> blogList = iBlogService.findAll();
-00
         if (blogList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -59,7 +58,7 @@ public class BlogRestController {
     public ResponseEntity<?> getListIdCategory(@PathVariable("id") int id) {
        List<Blog> blog = iBlogService.findBlogByCategory(id);
        if (blog == null) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(blog, HttpStatus.OK);
     }
